38bfd025f5c53fe06b185f739f52624d8fcec1b0
Yoshua Wuyts <yoshuawuyts@gmail.com>
2020-09-11 14:07:13 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//! Async broadcast channels
+//!
+//! # Examples
+//!
+//! ```
+//! // tbi
+//! ```
+
+#![forbid(unsafe_code, future_incompatible, rust_2018_idioms)]
+#![deny(missing_debug_implementations, nonstandard_style)]
+#![warn(missing_docs, missing_doc_code_examples, unreachable_pub)]
