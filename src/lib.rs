007e0dcc8e82f481b76fd660c24aef0c21b66b7b
Zeeshan Ali <zeeshanak@gnome.org>
2021-05-05 17:21:46 +0200

    Very basic tests for async API

--- src/lib.rs
+++ src/lib.rs
@@ -419,7 +419,7 @@ impl<'a, T: Clone> Future for Recv<'a, T> {
 }
 
 #[test]
-fn smoke() {
+fn sync() {
     let (s, mut r1) = broadcast(10);
     let mut r2 = r1.clone();
 
@@ -432,4 +432,25 @@ fn smoke() {
     assert_eq!(r1.try_recv().unwrap(), 8);
     assert_eq!(r2.try_recv().unwrap(), 8);
     assert_eq!(r3.try_recv().unwrap(), 8);
+}
+
+#[test]
+fn r#async() {
+    use futures_util::stream::StreamExt;
+
+    pollster::block_on(async {
+        let (s, mut r1) = broadcast(10);
+        let mut r2 = r1.clone();
+
+        s.broadcast(7).await.unwrap();
+        assert_eq!(r1.recv().await.unwrap(), 7);
+        assert_eq!(r2.recv().await.unwrap(), 7);
+
+        // Now let's try the Stream impl.
+        let mut r3 = r1.clone();
+        s.broadcast(8).await.unwrap();
+        assert_eq!(r1.next().await.unwrap(), 8);
+        assert_eq!(r2.next().await.unwrap(), 8);
+        assert_eq!(r3.next().await.unwrap(), 8);
+    });
 }
