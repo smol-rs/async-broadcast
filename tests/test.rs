6db6588795ad4da1b941a578d0f56c022b8bddad
Zeeshan Ali <zeeshanak@gnome.org>
2021-05-08 01:27:04 +0200

    tests: More reliable synchronization between threads
    Use std::sync::mpsc channel to synchronize sender and receiver threads,
    instead of arbitrary sleeps.

--- tests/test.rs
+++ tests/test.rs
@@ -1,15 +1,10 @@
-use std::thread::sleep;
-use std::time::Duration;
+use std::sync::mpsc;
 
 use futures_util::stream::StreamExt;
 use async_broadcast::*;
 
 use easy_parallel::Parallel;
 use futures_lite::future::block_on;
-
-fn ms(ms: u64) -> Duration {
-    Duration::from_millis(ms)
-}
 
 #[test]
 fn basic_sync() {
@@ -52,24 +47,35 @@ fn parallel() {
     let s2 = s1.clone();
     let mut r2 = r1.clone();
 
+    let (sender_sync_send, sender_sync_recv) = mpsc::channel();
+    let (receiver_sync_send, receiver_sync_recv) = mpsc::channel();
+
     Parallel::new()
         .add(move || {
-            sleep(ms(50));
+            sender_sync_recv.recv().unwrap();
+
             s1.try_broadcast(7).unwrap();
             s2.try_broadcast(8).unwrap();
-
             assert!(s2.try_broadcast(9).unwrap_err().is_full());
             assert!(s1.try_broadcast(10).unwrap_err().is_full());
+            receiver_sync_send.send(()).unwrap();
+
+            drop(s1);
+            drop(s2);
+            receiver_sync_send.send(()).unwrap();
         })
         .add(move || {
             assert_eq!(r1.try_recv(), Err(TryRecvError::Empty));
             assert_eq!(r2.try_recv(), Err(TryRecvError::Empty));
-            sleep(ms(50));
+            sender_sync_send.send(()).unwrap();
+
+            receiver_sync_recv.recv().unwrap();
             assert_eq!(r1.try_recv().unwrap(), 7);
             assert_eq!(r1.try_recv().unwrap(), 8);
             assert_eq!(r2.try_recv().unwrap(), 7);
             assert_eq!(r2.try_recv().unwrap(), 8);
-            sleep(ms(50));
+
+            receiver_sync_recv.recv().unwrap();
             assert_eq!(r1.try_recv(), Err(TryRecvError::Closed));
             assert_eq!(r2.try_recv(), Err(TryRecvError::Closed));
         })
@@ -82,36 +88,43 @@ fn parallel_async() {
     let s2 = s1.clone();
     let mut r2 = r1.clone();
 
+    let (sender_sync_send, sender_sync_recv) = mpsc::channel();
+    let (receiver_sync_send, receiver_sync_recv) = mpsc::channel();
+
     Parallel::new()
         .add(move || block_on(async move {
-            sleep(ms(50));
+            sender_sync_recv.recv().unwrap();
+
             s1.broadcast(7).await.unwrap();
             s2.broadcast(8).await.unwrap();
-
             assert!(s2.try_broadcast(9).unwrap_err().is_full());
             assert!(s1.try_broadcast(10).unwrap_err().is_full());
+            receiver_sync_send.send(()).unwrap();
+
             s1.broadcast(9).await.unwrap();
             s2.broadcast(10).await.unwrap();
-            sleep(ms(50));
+
+            drop(s1);
+            drop(s2);
+            receiver_sync_send.send(()).unwrap();
         }))
         .add(move || block_on(async move {
             assert_eq!(r1.try_recv(), Err(TryRecvError::Empty));
             assert_eq!(r2.try_recv(), Err(TryRecvError::Empty));
+            sender_sync_send.send(()).unwrap();
 
             assert_eq!(r1.next().await.unwrap(), 7);
             assert_eq!(r2.next().await.unwrap(), 7);
-
             assert_eq!(r1.recv().await.unwrap(), 8);
             assert_eq!(r2.recv().await.unwrap(), 8);
 
-            sleep(ms(50));
+            receiver_sync_recv.recv().unwrap();
             assert_eq!(r1.next().await.unwrap(), 9);
             assert_eq!(r2.next().await.unwrap(), 9);
 
             assert_eq!(r1.recv().await.unwrap(), 10);
             assert_eq!(r2.recv().await.unwrap(), 10);
 
-            sleep(ms(50));
             assert_eq!(r1.recv().await, Err(RecvError));
             assert_eq!(r2.recv().await, Err(RecvError));
         }))
